260fd77614d3a36121eae6eef6640980d7b6c4c8
jeanbaptisteguillois <jguilloi@amazon.fr>
2022-01-31 09:33:09 +0100

    Major app update to use several nodes

--- src/app.js
+++ src/app.js
@@ -16,37 +16,104 @@ COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER I
 TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
-// A basic node application leveraging the Express lib
 const express = require("express");
 const { networkInterfaces } = require('os');
+const awsFactory = require('aws-sdk');
+const ddb = require('./ddbClient.js');
 
+let nodeId = undefined;
 const app = express();
 const applicationHttpPort = process.env.CLUSTER_SAMPLE_APP_PORT || 3000;
+const currentAWSRegion = process.env.AWS_REGION || 'eu-west-3'
+
+awsFactory.config.update({region: currentAWSRegion});
 
 let mainPageHitCounter = 1;
 let healthCheckHitCounter = 0;
 
+// ***********************
+// Application initializer
+// ***********************
 var server = app.listen(applicationHttpPort, () => {
-  console.info("Cluster sample app started...");
-  console.info("Listening on port "+ applicationHttpPort);
+  // Get DynamoDB Client
+  ddb.init(awsFactory);
+
+  // Collect application node current ipV4 addresses
+  const ipv4Addrs = getAllIPAddrs();
+
+  // Set our node ID
+  nodeId = Math.random().toString(16).slice(2);
+
+  // Store our own node data in DynamoDB
+  ddb.saveNodeData(nodeId, ipv4Addrs, mainPageHitCounter, healthCheckHitCounter).then(() => {
+    console.info("Cluster sample app started...");
+    console.info("Listening on port "+ applicationHttpPort);
+  }).catch((error) => {
+    console.error('Unable to initialize application: ', error);
+  });
 });
 
-// Default REST handler (HTTP GET on /)
-app.get("/", (req, res, next) => {
+// ****************************
+// Process shutdown gracefully
+// ****************************
+process.on('SIGTERM', shutdown);
+process.on('SIGINT', shutdown);
+
+// ***********************
+// Server shutdown handler
+// ***********************
+function shutdown(signal) {
+  console.log('Shutting down from signal: ', signal);
+  ddb.cleanUpNodesData(nodeId).then(() => {
+    console.log('Closing server...');
+    server.close(() => {
+      console.log('Server closed')
+    })  
+  }).catch((error) => {
+    console.error('An error occured while cleaning up application nodes data in DynamoDB: ', error);
+  });
+}
+
+// **********************
+// Default REST handler
+// (HTTP GET on /)
+// **********************
+app.get("/", async (req, res, next) => {
   console.debug('Processing a GET request on / from host '+req.headers['host']);
-  res.send(getHTMLContent());
-  mainPageHitCounter+=1;
+
+  // Load all application nodes data from DynamoDB
+  ddb.getAllNodesData().then((nodesData) => {
+
+    // Build page content
+    res.send(getHTMLContent(nodesData, nodeId));
+
+    // Updater our counters and save new value
+    mainPageHitCounter+=1;
+    
+    ddb.updateNodeData(nodeId, mainPageHitCounter, healthCheckHitCounter);
+  }).catch((error) => {
+    console.error('An error occured while loading application nodes data from DynamoDB: ', error);
+    res.send('An error occured while loading application nodes data from DynamoDB: '+ error.toString());
+  })
 });
 
-// Default REST handler for the healthcheck (HTTP GET on /healtcheck)
-app.get("/healthcheck", (req, res, next) => {
+// *****************************************
+// Default REST handler for the healthcheck
+// (HTTP GET on /healtcheck)
+// *****************************************
+app.get("/healthcheck", async (req, res, next) => {
   console.debug('Processing a GET request on /healthcheck from host '+req.headers['host']);
   res.send("OK");
   healthCheckHitCounter+=1;
+  ddb.updateNodeData(nodeId, mainPageHitCounter, healthCheckHitCounter).catch((error) => {
+    console.error('Unable to update application nodes data in DyamoDB', error);
+  });
 });
 
+// *****************************************
 // Returns the HTML content of the main page
-function getHTMLContent() {
+// *****************************************
+function getHTMLContent(nodes, nodeId) {
   
   const dateLocaleOptions = {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: false, timeZoneName: 'short'};
 
@@ -54,14 +121,26 @@ function getHTMLContent() {
   htmlContent = htmlContent + '<body>';
   htmlContent = htmlContent + '<div style="display: inline-block; text-align: center; padding: 20px;"><h1>Greetings from Cluster Sample App!</h1>';
   htmlContent = htmlContent + `<h3>Today is ${new Date().toLocaleString('en-US', dateLocaleOptions)}`+'</h3>';
-  htmlContent = htmlContent + `<p>This web page has been hit ${mainPageHitCounter} time(s)</p>`;
-  htmlContent = htmlContent + `<p>The healthcheck of this application has been hit ${healthCheckHitCounter} time(s)</p>`;
+  htmlContent = htmlContent + `<p>This application node has been hit ${mainPageHitCounter} time(s)</p>`;
+  htmlContent = htmlContent + `<p>The healthcheck of this application node has been hit ${healthCheckHitCounter} time(s)</p>`;
 
-  htmlContent = htmlContent + '<div><p>This app is running in a container having the following IP addresses: ';
-  htmlContent = htmlContent + '<table text-align: center; border-style: solid;><th>Name</th><th>Type</th><th>CIDR</th><th>Address</th>';
-  getAllIPAddrs().forEach((ip) => {
-    htmlContent = htmlContent + `<tr><td>${ip.name}</td><td>${ip.infos.family}</td><td>${ip.infos.cidr}</td><td>${ip.infos.address}</td></tr>`;
+  htmlContent = htmlContent + `<div><p>This application cluster is made of ${nodes.length} nodes with the following properties:`;
+  // htmlContent = htmlContent + '<table text-align: center; border-style: solid;><th>Name</th><th>Type</th><th>CIDR</th><th>Address</th>';
+  htmlContent = htmlContent + '<table text-align: center; border-style: solid;><th>Application Node ID</th><th>#Page Hit</th><th>#Health Check Hit</th><th>IP Addresses</th>';
+
+  nodes.forEach((node) => {
+    const elem = JSON.parse(node.IP_ADDRS);
+    let ipAddrs = '';
+    elem.forEach(ipaddr => {
+      ipAddrs = ipAddrs + `${ipaddr.infos.address}\n`
+    });
+    if(node.NODE_ID === nodeId) {
+      htmlContent = htmlContent + `<tr style="background-color: #3fff00;"><td>${node.NODE_ID} (this node)</td><td>${node.PAGE_HIT_COUNT}</td><td>${node.HEALTHCHECK_HIT_COUNT}</td><td>${ipAddrs}</td></tr>`;
+    } else {
+      htmlContent = htmlContent + `<tr><td>${node.NODE_ID}</td><td>${node.PAGE_HIT_COUNT}</td><td>${node.HEALTHCHECK_HIT_COUNT}</td><td>${ipAddrs}</td></tr>`;  
+    }
   });
+
   htmlContent = htmlContent + '</table></p></div></body>';
 
   return htmlContent;
@@ -77,7 +156,9 @@ function getCSSString() {
   return css;
 }
 
+// ***************************************************
 // Computes a list of IPv4 addresses seen from the app
+// ***************************************************
 function getAllIPAddrs() {
 
   const ipv4Addrs = [];
---
+++ src/ddbClient.js
@@ -0,0 +1,125 @@
+/*
+** Cluster Sample Application - DynamoDB Client - https://github.com/aws-samples/cluster-sample-app 
+© 2021 Amazon Web Services, Inc. or its affiliates. All Rights Reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of
+the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+var ddbClient = undefined;
+
+function init(awsFactory) {
+    ddbClient = new awsFactory.DynamoDB.DocumentClient({apiVersion: '2012-08-10'});
+}
+
+async function saveNodeData(nodeId, ipv4Addrs, mainPageHitCounter, healthCheckHitCounter) {
+    return new Promise(function(resolve, reject) {
+        
+        const params = {
+            TableName: 'CLUSTER_SAMPLE_APP_NODE',
+            Item: {
+                'NODE_ID' : nodeId.toString(),
+                'IP_ADDRS' : JSON.stringify(ipv4Addrs),
+                'PAGE_HIT_COUNT': mainPageHitCounter.toString(),
+                'HEALTHCHECK_HIT_COUNT': healthCheckHitCounter.toString(),
+            }
+        };
+    
+        ddbClient.put(params, function(err, data) {
+            if (err) {
+                console.error("Unable to insert application data in DynamoDB: ", err);
+                reject(err);
+            } else {
+                console.log("Application data successfully inserted in DynamoDB");
+                resolve(data);
+            }
+        });
+    
+    });
+}
+
+async function cleanUpNodesData(nodeId) {
+    return new Promise(function(resolve, reject) {
+
+        const params = {
+            TableName: 'CLUSTER_SAMPLE_APP_NODE',
+            Key: {
+                'NODE_ID' : nodeId.toString()
+            }
+        };
+    
+        ddbClient.delete(params, function(err, data) {
+            if (err) {
+                console.error("Unable to delete application node data", err);
+                reject(err);
+            } else {
+                console.log("Successfully deleted application node data");
+                resolve(data);
+            }
+        });
+    });
+}
+
+async function getAllNodesData(nodeId) {
+    return new Promise(function(resolve, reject) {
+
+        let params = {
+            ExpressionAttributeValues: {
+              ':nodeid': {S: '1'}
+            },
+            FilterExpression: "NODE_ID <> :nodeid",
+            TableName: 'CLUSTER_SAMPLE_APP_NODE'
+          };
+        
+        ddbClient.scan(params, function(err, data) {
+            if (err) {
+                console.error("Error", err);
+                reject(err);
+            } else {
+                resolve(data.Items);
+            }
+        });
+
+    });
+}
+
+async function updateNodeData(nodeId, mainPageHitCounter, healthCheckHitCounter) {
+    return new Promise(function(resolve, reject) {
+        
+        const params = {
+            TableName: 'CLUSTER_SAMPLE_APP_NODE',
+            Key: { 'NODE_ID' : nodeId.toString() },
+            UpdateExpression: 'set PAGE_HIT_COUNT = :pageHit, HEALTHCHECK_HIT_COUNT = :healthHit',
+            ExpressionAttributeValues: {
+                ':pageHit' : mainPageHitCounter.toString(),
+                ':healthHit' : healthCheckHitCounter.toString()
+            }
+        };
+    
+        ddbClient.update(params, function(err, data) {
+            if (err) {
+                console.error("Unable to update application data in DynamoDB: ", err);
+                reject(err);
+            } else {
+                console.log("Application data successfully updated in DynamoDB");
+                resolve(data);
+            }
+        });
+    
+    });
+}
+exports.init = init
+exports.saveNodeData = saveNodeData
+exports.updateNodeData = updateNodeData
+exports.cleanUpNodesData = cleanUpNodesData
+exports.getAllNodesData = getAllNodesData
